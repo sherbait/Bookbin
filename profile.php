ce41a9da8d9dbd721abd4490f4fead50e5a82c5f
vanessapmak@gmail.com <Hrd2rmbr1>
2018-05-05 18:39:25 +0800

    added profile_sidebar.php

--- profile.php
+++ profile.php
@@ -2,19 +2,13 @@
     include "header.php";
     include "./php/session.php";
     include "./php/user_info.php";
+    include "profile_sidebar.php";
 ?>
 
 <!-- Code here to display profile information. Select whichever is relevant for this page. -->
-<div class="container mt-5">
+<div class="container">
     <div class="row">
-        <div class="col-sm-2">
-            <ul class="navbar-nav ml-auto">
-                <li><a href="#">Profile</a></li>
-                <li><a href="update_account.php">Account Settings</a></li>
-                <li><a href="#">Trade History</a></li>
-                <li><a href="#">Notifications</a></li>
-            </ul>
-        </div>
+        <div class="col-sm-2"></div>
         <div class="col-sm-10">
             <label>Username: </label><?php echo $username ?><br>
             <label>Bookpoints: </label><?php echo $bookpoint ?><br>
@@ -26,8 +20,8 @@
             <label>Address: </label><?php echo $address ?><br>
             <label>Member since: </label><?php echo date_format(date_create($created),"M Y") ?><br>
         </div>
-        </div>
-    </div>
+      </div>
 </div>
+
 
 <?php   include "footer.php"    ?>
---
+++ profile_sidebar.php
@@ -0,0 +1,13 @@
+
+    <div class="container mt-5">
+    <div class="row">
+        <div class="col-sm-2">
+            <ul class="navbar-nav ml-auto">
+                <li><a href="#">Profile</a></li>
+                <li><a href="update_account.php">Account Settings</a></li>
+                <li><a href="#">Trade History</a></li>
+                <li><a href="#">Notifications</a></li>
+            </ul>
+        </div>
+    </div>
+    </div>
